baa906e056f9a17793b959f65a1f865882e5d601
David Obodo <obododavid5@gmail.com>
2020-05-04 11:01:21 +0100

    refactor imports in server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express');
+import 'babel-polyfill';
+import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import App from './src/app';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
 app.get('*', (req, res) => {
+
+
     res.send('Thid is my appp')
 })
 app.listen(PORT, () => {
