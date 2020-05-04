52c86dcd5982da8f1d901808fed9f68b31a8f754
David Obodo <obododavid5@gmail.com>
2020-05-04 10:59:01 +0100

    rendering content on browser from server

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.get('*', (req, res) => {
+    res.send('Thid is my appp')
+})
 app.listen(PORT, () => {
     console.log(`App running ${PORT}`)
 })
