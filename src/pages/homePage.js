f7fd6407057fbd108b70e7d6519f8a14a450ed65
David Obodo <obododavid5@gmail.com>
2020-05-04 12:06:36 +0100

    add helmet to client side

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const HomePage = () => {
 
@@ -6,8 +7,17 @@ const HomePage = () => {
         console.log('JS is running')
     }
 
+    const head = () => {
+        return (
+            <Helmet>
+                <title>My Page title</title>
+            </Helmet>
+        )
+    }
+
     return (
         <div>
+            {head()}
             <h1>My home page</h1>
             <p>Some content</p>
             <button onClick={handleOnclick}>Console log some text</button>
