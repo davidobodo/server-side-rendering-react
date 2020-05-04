cab3fb189623b9936b955fa0e402e62f2c2c99af
David Obodo <obododavid5@gmail.com>
2020-05-04 11:48:27 +0100

    add click event to home page

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const HomePage = () => {
+
+    const handleOnclick = () => {
+        console.log('JS is running')
+    }
+
     return (
         <div>
             <h1>My home page</h1>
             <p>Some content</p>
+            <button onClick={handleOnclick}>Console log some text</button>
         </div>
     )
 }
