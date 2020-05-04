f917a7c0f488563c1a7aa250b9849cc40690e988
David Obodo <obododavid5@gmail.com>
2020-05-04 11:54:51 +0100

    refactor server.js in order to protect servers bundle file

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('build'));
+app.use(express.static('build/public'));
 
 
 app.get('*', (req, res) => {
